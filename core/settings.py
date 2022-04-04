6a2dfe0c17e9f6fa3c5a2fd5b59ebd49a6a9ec48
Mahdi Amiri <mahdi_amiri98@comp.iust.ac.ir>
2022-04-04 23:36:11 +0430

    Database change

--- core/settings.py
+++ core/settings.py
@@ -83,26 +83,26 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#   'default': {
-#       'ENGINE': 'django.db.backends.mysql',
-#       'NAME': '98522148$projdb',
-#       'HOST': '98522148.mysql.pythonanywhere-services.com',
-#       'USER': '98522148',
-#       'PASSWORD': 'admin2000',
-#   }
-# }
-
 DATABASES = {
   'default': {
-      'ENGINE': 'django.db.backends.postgresql_psycopg2',
-      'NAME': 'projdb',
-      'HOST': 'localhost',
-      'USER': 'postgres',
+      'ENGINE': 'django.db.backends.mysql',
+      'NAME': '98522148$projdb',
+      'HOST': '98522148.mysql.pythonanywhere-services.com',
+      'USER': '98522148',
       'PASSWORD': 'admin2000',
-      'PORT': '5432',
   }
 }
+
+# DATABASES = {
+#   'default': {
+#       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#       'NAME': 'projdb',
+#       'HOST': 'localhost',
+#       'USER': 'postgres',
+#       'PASSWORD': 'admin2000',
+#       'PORT': '5432',
+#   }
+# }
 
 
 # Password validation
