98e9201c989b4df97c22b874e34223ceef102cc3
Mahdi Amiri <mahdi_amiri98@comp.iust.ac.ir>
2022-04-21 13:12:35 +0430

    add trailing slash

--- read_book/urls.py
+++ read_book/urls.py
@@ -4,8 +4,8 @@ from .views import NewestBooks, PDFRetrieval, BookInfoRetrieval, AllBooks
 app_name = 'read_book'
 
 urlpatterns = [
-    path('newest_books', NewestBooks.as_view()),
-    path('all_books', AllBooks.as_view()),
+    path('newest_books/', NewestBooks.as_view()),
+    path('all_books/', AllBooks.as_view()),
     path('pdf_file/<id>', PDFRetrieval.as_view()),    
     path('info/<id>', BookInfoRetrieval.as_view()),
 ]
