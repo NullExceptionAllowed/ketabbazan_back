2ca0cdb441ad4d3e5c0b89185a1bcc3c37433da1
Mahdi Amiri <mahdi_amiri98@comp.iust.ac.ir>
2022-04-21 23:52:26 +0430

    add book foreign key to Article

--- write_article/models.py
+++ write_article/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from read_book.models import Book
 
 def upload_to(instance, filename):
     return 'articles/{filename}'.format(filename=filename)
@@ -12,6 +13,7 @@ class Article(models.Model):
     summary = models.TextField(blank=True, default='')
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True, on_delete=models.CASCADE)
     image = models.ImageField(_("Image"), upload_to=upload_to, default='articles/default.jpg')
+    book = models.ForeignKey(Book, blank=True, null=True, on_delete = models.SET_NULL)
 
     class Meta:
         ordering = ['created']
