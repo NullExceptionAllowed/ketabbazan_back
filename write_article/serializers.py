95b9df72e3191fe8cb45584ca84d80d45c479ec9
Mahdi Amiri <mahdi_amiri98@comp.iust.ac.ir>
2022-04-23 15:33:07 +0430

    display nickname of owner

--- write_article/serializers.py
+++ write_article/serializers.py
@@ -2,8 +2,8 @@ from rest_framework import serializers
 from write_article.models import Article
 
 class ArticleSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')
+    owner = serializers.ReadOnlyField(source='owner.nickname')
 
     class Meta:
         model = Article
-        fields = ['id', 'title', 'image', 'book', 'body', 'summary', 'owner']
+        fields = ['id', 'title', 'image', 'book', 'created', 'body', 'summary', 'owner']
