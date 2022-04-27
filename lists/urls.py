a54af8787599cd1831023327a146ad27f29eb836
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2022-04-27 21:12:06 +0430

    compelete lists

--- lists/urls.py
+++ lists/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import Addtopast_read
+from .views import user_cur_read, user_past_read, user_favourite
 from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
-    path('pastread/', Addtopast_read.as_view()),
+    path('pastread/', user_past_read.as_view()),
+    path('curread/', user_cur_read.as_view()),
+    path('favourite/', user_favourite.as_view())
 
 ]
--- lists/views.py
+++ lists/views.py
@@ -9,29 +9,82 @@ from accounts.models import User
 # Create your views here.
 
 
-class Addtopast_read(APIView):
+def all_books(books):
+    ans = []
+    for book in books:
+        book_serializer = BookInfoSerializer(instance=book)
+        data = book_serializer.data
+        data['id'] = book.id
+        data['author'] = "، ".join(str(author) for author in book.author.all())
+        ans.append(data)
+    return ans
+
+class user_past_read(APIView):
     permission_classes = [IsAuthenticated, ]
 
     def post(self, request):
         book_id = request.data['book']
         book = Book.objects.get(id=book_id)
+        if request.user.cur_read.filter(id=book_id):
+            request.user.cur_read.remove(book)
         request.user.past_read.add(book)
         request.user.save()
         return Response(status=status.HTTP_200_OK )
 
     def get(self, request):
         books = [book for book in request.user.past_read.all()]
-        ans = []
-        for book in books:
-            book_serializer = BookInfoSerializer(instance=book)
-            data = book_serializer.data
-            data['id'] = book.id
-            data['author'] = "، ".join(str(author) for author in book.author.all())
-            ans.append(data)
-        return Response(ans)
+        ans = all_books(books)
+        return Response(ans, status=status.HTTP_200_OK)
 
     def delete(self, request):
         book_id = request.data['book']
         book = Book.objects.get(id=book_id)
         request.user.past_read.remove(book)
         return Response(status=status.HTTP_200_OK)
+
+
+class user_cur_read(APIView):
+    permission_classes = [IsAuthenticated, ]
+
+    def post(self, request):
+        book_id = request.data['book']
+        book = Book.objects.get(id=book_id)
+        if request.user.past_read.filter(id=book_id):
+            request.user.past_read.remove(book)
+        request.user.cur_read.add(book)
+        request.user.save()
+        return Response(status=status.HTTP_200_OK )
+
+    def get(self, request):
+        books = [book for book in request.user.past_read.all()]
+        ans = all_books(books)
+        return Response(ans, status=status.HTTP_200_OK)
+
+    def delete(self, request):
+        book_id = request.data['book']
+        book = Book.objects.get(id=book_id)
+        request.user.cur_read.remove(book)
+        return Response(status=status.HTTP_200_OK)
+
+
+class user_favourite(APIView):
+    permission_classes = [IsAuthenticated, ]
+
+    def post(self, request):
+        book_id = request.data['book']
+        book = Book.objects.get(id=book_id)
+        request.user.favourite.add(book)
+        request.user.save()
+        return Response(status=status.HTTP_200_OK)
+
+    def get(self, request):
+        books = [book for book in request.user.favourite.all()]
+        ans = all_books(books)
+        return Response(ans, status=status.HTTP_200_OK)
+
+    def delete(self, request):
+        book_id = request.data['book']
+        book = Book.objects.get(id=book_id)
+        request.user.favourite.remove(book)
+        return Response(status=status.HTTP_200_OK)
+
