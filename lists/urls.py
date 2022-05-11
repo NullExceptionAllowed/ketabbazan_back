178d39f2f378d683ad45f8ae21fa656c1f255edf
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2022-05-12 01:43:22 +0430

    good change in code for add to list and get book of a list

--- lists/urls.py
+++ lists/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
-from .views import user_cur_read, user_past_read, user_favourite, favourite_anyway, pastread_anyway, curread_anyway
+#from .views import user_cur_read, user_past_read, user_favourite, favourite_anyway, pastread_anyway, curread_anyway, test
 from rest_framework.routers import DefaultRouter
+from .views import get_books_of_list
+router = DefaultRouter()
+router.register('test', get_books_of_list)
+
 
 urlpatterns = [
-    path('pastread/', user_past_read.as_view()),
-    path('curread/', user_cur_read.as_view()),
-    path('favourite/', user_favourite.as_view()),
-    path('forcepastread/', pastread_anyway.as_view()),
-    path('forcecurread/', curread_anyway.as_view()),
-    path('forcefavourite/', favourite_anyway.as_view())
+    #path('pastread/', user_past_read.as_view()),
+   # path('curread/', user_cur_read.as_view()),
+   # path('favourite/', user_favourite.as_view()),
+   # path('forcepastread/', pastread_anyway.as_view()),
+   # path('forcecurread/', curread_anyway.as_view()),
+   # path('forcefavourite/', favourite_anyway.as_view()),
+
 ]
+urlpatterns += router.urls
--- lists/views.py
+++ lists/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, viewsets
+
+import accounts.serializers
 from read_book.models import Book
 from read_book.serializers import BookInfoSerializer
 from accounts.models import User
+
+
 # Create your views here.
 
 
@@ -20,146 +25,39 @@ def all_books(books):
     return ans
 
 
-class user_past_read(APIView):
-    permission_classes = [IsAuthenticated, ]
-
+class add_to_list(APIView):
     def post(self, request):
-        book_id = request.data['book']
+        try:
+            list_id = request.data['list_id']
+        except:
+            return Response({"message": "select a list"}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            book_id = request.data['book_id']
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
         book = Book.objects.get(id=book_id)
-        if request.user.cur_read.filter(id=book_id).exists():
-            return Response({"message": "this book is in cur read list"}, status=status.HTTP_400_BAD_REQUEST)
-        elif request.user.favourite.filter(id=book_id).exists():
-            return Response({"message": "this book in favourite list"}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        request.user.past_read.add(book)
-        request.user.save()
-        return Response({"message": "book add successfully"}, status=status.HTTP_200_OK)
-
-    def get(self, request):
-        books = [book for book in request.user.past_read.all()]
-        ans = all_books(books)
-        return Response(ans, status=status.HTTP_200_OK)
-
-    def delete(self, request):
-        book_id = request.data['book']
-        book = Book.objects.get(id=book_id)
-        request.user.past_read.remove(book)
+        if list_id == 1:
+            request.user.past_read.add(book)
+        elif list_id == 2:
+            request.user.cur_read.add(book)
+        elif list_id == 3:
+            request.user.favourite.add(book)
+        elif list_id == 4:
+            request.user.left_read.add(book)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
         return Response(status=status.HTTP_200_OK)
 
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 1
+    page_size_query_param = 'page_size'
+    max_page_size = 10
 
-class user_cur_read(APIView):
-    permission_classes = [IsAuthenticated, ]
+class get_books_of_list(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookInfoSerializer
+    pagination_class = StandardResultsSetPagination
 
-    def post(self, request):
-        book_id = request.data['book']
-        book = Book.objects.get(id=book_id)
-        if request.user.past_read.filter(id=book_id).exists():
-            return Response({"message": "this book is in past read list"}, status=status.HTTP_406_NOT_ACCEPTABLE)
-        elif request.user.favourite.filter(id=book_id).exists():
-            return Response({"message": "this book is in cur read list"}, status=status.HTTP_400_BAD_REQUEST)
-        request.user.cur_read.add(book)
-        request.user.save()
-        return Response({"message": "book add successfully"}, status=status.HTTP_200_OK)
+    def list(self, request, *args, **kwargs):
+        return super().list(request.user.past_read.all())
 
-    def get(self, request):
-        books = [book for book in request.user.cur_read.all()]
-        ans = all_books(books)
-        return Response(ans, status=status.HTTP_200_OK)
-
-    def delete(self, request):
-        book_id = request.data['book']
-        book = Book.objects.get(id=book_id)
-        request.user.cur_read.remove(book)
-        return Response(status=status.HTTP_200_OK)
-
-
-class user_favourite(APIView):
-    permission_classes = [IsAuthenticated, ]
-
-    def post(self, request):
-        book_id = request.data['book']
-        book = Book.objects.get(id=book_id)
-        if request.user.past_read.filter(id=book_id).exists():
-            return Response({"message": "this book is in past read list"}, status=status.HTTP_406_NOT_ACCEPTABLE)
-        elif request.user.cur_read.filter(id=book_id).exists():
-            return Response({"message": "this book is in cur read list"}, status=status.HTTP_400_BAD_REQUEST)
-        request.user.favourite.add(book)
-        request.user.save()
-        return Response({"message": "book add successfully"}, status=status.HTTP_200_OK)
-
-    def get(self, request):
-        books = [book for book in request.user.favourite.all()]
-        ans = all_books(books)
-        return Response(ans, status=status.HTTP_200_OK)
-
-    def delete(self, request):
-        book_id = request.data['book']
-        book = Book.objects.get(id=book_id)
-        request.user.favourite.remove(book)
-        return Response(status=status.HTTP_200_OK)
-
-class user_left_read(APIView):
-    permission_classes = [IsAuthenticated, ]
-
-    def post(self, request):
-        book_id = request.data['book']
-        book = Book.objects.get(id=book_id)
-        if request.user.cur_read.filter(id=book_id).exists():
-            return Response({"message": "this book is in cur read list"}, status=status.HTTP_400_BAD_REQUEST)
-        elif request.user.favourite.filter(id=book_id).exists():
-            return Response({"message": "this book in favourite list"}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        request.user.past_read.add(book)
-        request.user.save()
-        return Response({"message": "book add successfully"}, status=status.HTTP_200_OK)
-
-    def get(self, request):
-        books = [book for book in request.user.past_read.all()]
-        ans = all_books(books)
-        return Response(ans, status=status.HTTP_200_OK)
-
-    def delete(self, request):
-        book_id = request.data['book']
-        book = Book.objects.get(id=book_id)
-        request.user.past_read.remove(book)
-        return Response(status=status.HTTP_200_OK)
-
-
-class pastread_anyway(APIView):
-    permission_classes = [IsAuthenticated, ]
-
-    def post(self, request):
-        book_id = request.data['book']
-        book = Book.objects.get(id=book_id)
-        if request.user.favourite.filter(id=book_id).exists():
-            request.user.favourite.remove(book)
-        elif request.user.cur_read.filter(id=book_id).exists():
-            request.user.cur_read.remove(book)
-        request.user.past_read.add(book)
-        return Response(status=status.HTTP_200_OK)
-
-
-class curread_anyway(APIView):
-    permission_classes = [IsAuthenticated, ]
-
-    def post(self, request):
-        book_id = request.data['book']
-        book = Book.objects.get(id=book_id)
-        if request.user.past_read.filter(id=book_id).exists():
-            request.user.past_read.remove(book)
-        elif request.user.favourite.filter(id=book_id).exists():
-            request.user.favourite.remove(book)
-        request.user.cur_read.add(book)
-        return Response(status=status.HTTP_200_OK)
-
-
-class favourite_anyway(APIView):
-    permission_classes = [IsAuthenticated, ]
-
-    def post(self, request):
-        book_id = request.data['book']
-        book = Book.objects.get(id=book_id)
-        if request.user.past_read.filter(id=book_id).exists():
-            request.user.past_read.remove(book)
-        elif request.user.cur_read.filter(id=book_id).exists():
-            request.user.cur_read.remove(book)
-        request.user.favourite.add(book)
-        return Response(status=status.HTTP_200_OK)
