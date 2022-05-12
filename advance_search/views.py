790e891e9a990c6e116e120a313976500e3d2286
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2022-05-12 16:01:30 +0430

    add pagination to advance search

--- advance_search/views.py
+++ advance_search/views.py
@@ -1,10 +1,16 @@
 from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
 from read_book.serializers import BookInfoSerializer
 from read_book.models import Book
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 # Create your views here.
 
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 8
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 class Advancesearch(generics.ListAPIView):
     queryset = Book.objects.all()
@@ -13,6 +19,7 @@ class Advancesearch(generics.ListAPIView):
     filterset_fields = {'name': ['contains'], 'author__name': ['contains'], 'genre__name': ['contains'],
                         'price': ['gte', 'lte'], 'publisher': ['contains']}
     ordering_fields = ['price', 'created']
+    pagination_class = StandardResultsSetPagination
 
     def list(self, request, *args, **kwargs):
         objs = super().list(request, *args, **kwargs)
