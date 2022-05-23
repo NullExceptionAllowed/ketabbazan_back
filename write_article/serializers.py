3715fb0ab0e1e521dab971c8f43b1bb5f2420091
Mahdi Amiri <mahdi_amiri98@comp.iust.ac.ir>
2022-05-23 23:35:54 +0430

    Update serializers.py

--- write_article/serializers.py
+++ write_article/serializers.py
@@ -3,6 +3,12 @@ from write_article.models import Article
 
 class ArticleSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.nickname')
+    owner_id = serializers.ReadOnlyField(source='owner.id')
+
+    image = serializers.SerializerMethodField()
+
+    def get_image(self, obj):
+        return "http://94.101.185.252" + obj.image.url
 
     class Meta:
         model = Article
