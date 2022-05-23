1be326749d13ab9ba223ff9d4d9428faf7ed67e3
Mahdi Amiri <mahdi_amiri98@comp.iust.ac.ir>
2022-05-23 23:34:18 +0430

    article image url corrected according to server base url

--- write_article/serializers.py
+++ write_article/serializers.py
@@ -5,6 +5,11 @@ class ArticleSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.nickname')
     owner_id = serializers.ReadOnlyField(source='owner.id')
 
+    image = serializers.SerializerMethodField()
+
+    def get_image(self, obj):
+        return "http://94.101.185.252" + obj.image.url
+
     class Meta:
         model = Article
         fields = ['id', 'title', 'image', 'book', 'created_jalali', 'body', 'summary', 'owner', 'owner_id']
