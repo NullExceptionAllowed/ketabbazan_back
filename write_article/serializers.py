3184fed884eede6751e73e046667a3141d9fa21b
Mahdi Amiri <mahdi_amiri98@comp.iust.ac.ir>
2022-07-06 06:14:09 +0000

    URL of owner image added to article APIs

--- write_article/serializers.py
+++ write_article/serializers.py
@@ -1,18 +1,24 @@
 from rest_framework import serializers
 from write_article.models import Article
+from userprofile.models import Profile
+from accounts.models import User
 
 class ArticleSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.nickname')
     owner_id = serializers.ReadOnlyField(source='owner.id')
 
     image = serializers.SerializerMethodField()
+    owner_image = serializers.SerializerMethodField()
 
     def get_image(self, obj):
         return "https://api.ketabbazan.ml" + obj.image.url
 
+    def get_owner_image(self, obj):
+        return "https://api.ketabbazan.ml/profile/getimage/?username=" + obj.owner.username
+
     class Meta:
         model = Article
-        fields = ['id', 'title', 'image', 'book', 'created_jalali', 'body', 'summary', 'owner', 'owner_id']
+        fields = ['id', 'title', 'image', 'book', 'created_jalali', 'body', 'summary', 'owner', 'owner_id', 'owner_image']
 
 class ArticleSerializerUpload(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.nickname')
