f599a65cab0752ece5bc36ebae6b2fb5ba3144af
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2023-03-14 19:37:40 +0330

    UserActivity model created

--- show_profile/models.py
+++ show_profile/models.py
@@ -1,3 +1,11 @@
+import jinja2.meta
 from django.db import models
-
+from accounts.models import User
 # Create your models here.
+
+
+class UserActivity(models.Model):
+    user = models.ManyToOneRel(User, on_delete=models.CASCADE())
+    data = models.DateTimeField(auto_now=True)
+    type = models.CharField(max_length=10)
+    action_id = models.IntegerField()
