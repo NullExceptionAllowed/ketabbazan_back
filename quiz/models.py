b0a8fcceea1fd1659ce1eea36c2dbf5a959c25f6
Hossein <you@example.com>
2023-04-07 01:56:35 +0330

    Url and Model have been added

--- quiz/models.py
+++ quiz/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+
+from accounts.models import User
 from read_book.models import Book
+
 
 class Question(models.Model):
     question = models.CharField(max_length=1000, null=True)
@@ -13,8 +16,20 @@ class Question(models.Model):
     def __str__(self):
         return self.question
 
+
 class Quiz(models.Model):
     question = models.ManyToManyField(Question, blank=True)
 
     def __str__(self):
         return "Quiz" + str(self.id)
+
+
+class QuizResult(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    score = models.PositiveSmallIntegerField()
+    questions_count = models.PositiveSmallIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Score :: {str(self.score)} from {str(self.questions_count)} questions"
--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ObjectDoesNotExist
 from requests import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
@@ -5,7 +6,8 @@ from quiz.serializers import QuestionSerializer
 from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Question, Quiz
+from .models import Question, Quiz, QuizResult
+
 
 class SubmitQuiz(APIView):
     permission_classes = (IsAuthenticated,)
@@ -15,7 +17,11 @@ class SubmitQuiz(APIView):
         questions = Quiz.objects.get(id=quiz_id).question.order_by('id')
         for question, i in zip(questions, range(1, questions.count() + 1)):
             res["ans" + str(i)] = question.ans
+
+        # TODO :: create related quiz result model
+
         return Response(res)
+
 
 class GenerateQuiz(APIView):
     permission_classes = (IsAuthenticated,)
@@ -32,11 +38,11 @@ class GenerateQuiz(APIView):
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
         if (request.user.past_read.filter(id=book_id).count() == 0 and
-            request.user.cur_read.filter(id=book_id).count() == 0 and
-            request.user.favourite.filter(id=book_id).count() == 0 and
-            request.user.left_read.filter(id=book_id).count() == 0):
+                request.user.cur_read.filter(id=book_id).count() == 0 and
+                request.user.favourite.filter(id=book_id).count() == 0 and
+                request.user.left_read.filter(id=book_id).count() == 0):
             return Response(status=status.HTTP_403_FORBIDDEN)
-        
+
         queryset = Question.objects.filter(book=book_id).order_by('?')[:5]
 
         ans = []
@@ -56,6 +62,7 @@ class GenerateQuiz(APIView):
 
         return Response(ans)
 
+
 class ProposeQuestion(APIView):
     permission_classes = (IsAuthenticated,)
 
@@ -66,4 +73,40 @@ class ProposeQuestion(APIView):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def perform_create(self, serializer):
-        serializer.save()        
+        serializer.save()
+
+
+class BestScores(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        try:
+            results = QuizResult.objects.raw("""
+                SELECT
+                    accounts_user.id,
+                    accounts_user.nickname,
+                    profile.fullname,
+                    profile.gender,
+                    profile.image,
+                    SUM((res.score * res.questions_count)/100 ) AS score
+                FROM accounts_user , userprofile_profile as profile ,quiz_quizresult as res
+                WHERE
+                    accounts_user.id = res.user_id
+                    AND
+                    accounts_user.profile_id = profile.id
+                GROUP BY
+                    accounts_user.id,
+                    accounts_user.nickname,
+                    profile.fullname,
+                    profile.gender,
+                    profile.image
+                ORDER BY score DESC
+                LIMIT 30
+            """)
+        except QuizResult.DoesNotExist:
+            results = []
+
+        for result in results:
+            print(result)
+
+        return Response(results)
