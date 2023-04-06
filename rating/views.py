936d08ee27f36ee8cd7f97d24637608cddaf6640
Hossein <you@example.com>
2023-04-06 23:38:15 +0330

    Update rate has been added

--- rating/views.py
+++ rating/views.py
@@ -7,45 +7,46 @@ from read_book.models import Book
 from .serializers import Rateserializer, returnrateserializer
 from .models import Rating
 from rest_framework.response import Response
-from rest_framework import  status
+from rest_framework import status
 
 
 class Rate(APIView):
     permission_classes = [IsAuthenticated, ]
 
-    def post(self,request):
-        if not Book.objects.filter(id=request.data['book'], rating__user=request.user).exists():
-            request.data['user'] = request.user.id
-            ser_rate = Rateserializer(data=request.data)
-            if (ser_rate.is_valid()):
-                ser_rate.save()
-                return Response(ser_rate.data, status=status.HTTP_200_OK)
-            else:
-                return Response(ser_rate.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request):
+        book_id = request.data['book']
+        user_id = request.user.id
+        old_rate = Rating.objects.filter(user_id=user_id, book_id=book_id).first()
+        if old_rate is not None:
+            old_rate.delete()
+
+        request.data['user'] = request.user.id
+        ser_rate = Rateserializer(data=request.data)
+        if (ser_rate.is_valid()):
+            ser_rate.save()
+            return Response(ser_rate.data, status=status.HTTP_200_OK)
         else:
-            return Response({"message": "you have rated for this book"},status=status.HTTP_400_BAD_REQUEST)
-
-
+            return Response(ser_rate.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class Userrate(APIView):
     permission_classes = [IsAuthenticated, ]
-    def get(self,request):
+
+    def get(self, request):
         book_id = request.query_params['book']
         book = Book.objects.get(id=book_id)
         try:
             rate = Rating.objects.get(book=book, user=request.user).rate
-            return Response({"rate":{"rate":rate}}, status=status.HTTP_200_OK)
+            return Response({"rate": {"rate": rate}}, status=status.HTTP_200_OK)
         except:
-            return Response({"rate":{"rate":None}}, status=status.HTTP_200_OK)
+            return Response({"rate": {"rate": None}}, status=status.HTTP_200_OK)
 
 
 class Getrate(APIView):
     permission_classes = [AllowAny, ]
-    def get(self,request):
+
+    def get(self, request):
         book_id = request.query_params['id']
         book = Book.objects.get(id=book_id)
         ser_book = returnrateserializer(book)
-        return Response({"rateinfo":ser_book.data}, status=status.HTTP_200_OK)
-
-
+        return Response({"rateinfo": ser_book.data}, status=status.HTTP_200_OK)
