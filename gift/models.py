2862f14aa7bb5b00538f1f377a5e3581cdeb3d7a
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2023-04-14 17:16:49 +0430

    gift model created

--- gift/models.py
+++ gift/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from accounts.models import User
+from read_book.models import Book
 # Create your models here.
+
+
+class GiftHistory(models.Model):
+    sender = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='receiver')
+    book = models.ForeignKey(to=Book, on_delete=models.CASCADE)
+    is_read = models.BooleanField(default=False)
