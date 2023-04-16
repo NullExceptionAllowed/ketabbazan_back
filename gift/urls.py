40a954c7ac44a172df6a553ee1c1a62cc120a572
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2023-04-16 23:47:21 +0430

    compelte urls.py

--- gift/urls.py
+++ gift/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import SendBook, ShowReceivedGift, ShowSendGift
+from .views import SendBook, ShowReceivedGift, ShowSendGift, HasUnreadMessage
 
 urlpatterns = [
     path('sendbook/', SendBook.as_view()),
     path('allreceivedgifts/', ShowReceivedGift.as_view()),
-    path('allsendgifts/', ShowSendGift.as_view())
+    path('allsendgifts/', ShowSendGift.as_view()),
+    path('hasunread/', HasUnreadMessage.as_view())
 ]
